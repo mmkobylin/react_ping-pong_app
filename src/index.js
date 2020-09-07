217bb3804465140fb88f13643b35352e26ad32c0
mmkobylin <mmkobylin@gmail.com>
2020-09-07 15:17:00 +0100

    initial scores showing up

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { createStore } from "redux";
 
   const initial = {
     player1: 0,
-    player2: 0,
+    player2: 3,
   };
 
   const reducer = (state, action ) => {
     switch (action.type) {
-      case "INCREMENT": return {...state, value: state.value + 1 } ;
       default: return state;
     }
   }
@@ -23,19 +22,20 @@ import { createStore } from "redux";
     window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__(),
     );
-    
-  store.subscribe(() => {
+//called whenever the state changes 
+  const render = () => {
     let state = store.getState();
-    console.log(state.count)
-  });
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+    ReactDOM.render(
+      <React.StrictMode>
+        <App player1 = { state.player1 } player2 = { state.player2 } />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
 
+  store.subscribe(render);
+  render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
