a6787b857a840e03af693988939f53dd931d8acb
mmkobylin <mmkobylin@gmail.com>
2020-09-07 15:27:05 +0100

    player1 score changed

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ import { createStore } from "redux";
 
   const initial = {
     player1: 0,
-    player2: 3,
+    player2: 0,
   };
 
   const reducer = (state, action ) => {
     switch (action.type) {
+      case "PLAYER1_SCORES": return { ...state, player1: state.player1 + 1 }
       default: return state;
     }
   }
-
+//add action and event handler; 
   const store = createStore(
     reducer,
     initial,
@@ -28,13 +29,22 @@ import { createStore } from "redux";
 
     ReactDOM.render(
       <React.StrictMode>
-        <App player1 = { state.player1 } player2 = { state.player2 } />
+        <App player1 = { state.player1 } player2 = { state.player2 }
+        count = { state.count }
+        handleIncrement = { () => store.dispatch( { type: "PLAYER1_SCORES" } ) } />
       </React.StrictMode>,
       document.getElementById('root')
     );
   }
 
-  store.subscribe(render);
+  store.subscribe(() => {
+    let state = store.getState();
+    console.log(state.player2)
+  });
+
+  store.dispatch( { type: "PLAYER1_SCORES" })
+  store.dispatch( { type: "PLAYER1_SCORES" })
+
   render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
